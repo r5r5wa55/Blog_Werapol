45589af4bd507ce3283cf5be560e81a73cfcbed5
werapol <r5r5wa55@hotmail.com>
2024-01-24 15:33:39 +0700

    connect-data-mongos

--- api/index.js
+++ api/index.js
@@ -1,5 +1,18 @@
 import  express  from "express";
+import mongoose from "mongoose";
+import dotenv from "dotenv";
+
+
+dotenv.config();
 const app = express();
+
+
+mongoose.connect(process.env.MONGO)
+.then(()=>{
+    console.log("mongodb");})
+.catch((err)=>{
+    onsole.log(err);
+})
 
 app.listen(3000,()=>{
     console.log("werapol");
