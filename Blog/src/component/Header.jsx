3fe3c94fc776251f3c85848e0dacbc2a6f11fc39
werapol <r5r5wa55@hotmail.com>
2024-02-01 14:11:31 +0700

    header show data user

--- Blog/src/component/Header.jsx
+++ Blog/src/component/Header.jsx
@@ -1,10 +1,14 @@
-import { Button, Navbar ,TextInput } from "flowbite-react";
+import { Avatar, Button, Dropdown, Navbar ,TextInput } from "flowbite-react";
 import { Link ,useLocation } from "react-router-dom";
 import { AiOutlineSearch } from "react-icons/ai"; 
 import {FaMoon} from 'react-icons/fa';
-
+import { useSelector } from 'react-redux';
+ 
 export default function Header() {
   const path = useLocation().pathname;
+  const {currenUser} = useSelector(state => state.user)
+
+
   return (
     <Navbar className="border-b-2">
         <Link to="/" className="self-center whitespace-nowrap text-sm sm:text-xl font-semibold dark:text-white ">
@@ -18,13 +22,34 @@ export default function Header() {
             placeholder="Search..."
             rightIcon={AiOutlineSearch}>
           </TextInput>
-          <Button className=" lg:hidden w-12 h-10" color="gray" pill><AiOutlineSearch/></Button>
         </form>
-        <Link className="flex gap-2 md:order-2">
+  
+        <Button className=" lg:hidden w-12 h-10" color="gray" pill><AiOutlineSearch/></Button>
+        <div className="flex gap-2 md:order-2">
           <Button className="w-12 h-10 sm:inline hidden" color="gray" pill><FaMoon /></Button>
-          <Button gradientDuoTone='purpleToBlue' outline >Sign In</Button>
+          {currenUser?
+            (<Dropdown arrowIcon={false}
+            inline
+            label={<Avatar alt="user" img={currenUser.profilePicture}></Avatar>}>
+              <Dropdown.Header>
+                <span className="block m-x text-sm ">{currenUser.username}</span>
+                <span className="block m-x text-sm truncate">{currenUser.email}</span>
+              </Dropdown.Header>
+                <Link to='/dashbord?tab=profile'>
+                  <Dropdown.Item>Profile</Dropdown.Item>
+                </Link>
+                <Dropdown.Divider />
+                <Dropdown.Item>Sign Out</Dropdown.Item>
+                
+            </Dropdown>):
+            (<Link className="flex gap-2 md:order-2">
+              <Button gradientDuoTone='purpleToBlue' outline >Sign In</Button>
+            </Link>)
+          }
+   
           <Navbar.Toggle />
-        </Link>
+        </div>
+  
         <Navbar.Collapse>
           <Navbar.Link active={path==="/"} as={'div'}>
               <Link to='/'>Home</Link>
