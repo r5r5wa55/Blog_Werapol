8f3effee1f511b893b8eda8b10dfd76be835e00c
werapol <r5r5wa55@hotmail.com>
2024-02-06 16:39:58 +0700

    user img upload

--- Blog/src/component/DashProfile.jsx
+++ Blog/src/component/DashProfile.jsx
@@ -1,19 +1,130 @@
-import { Button, TextInput } from "flowbite-react";
+import { Alert, Button, TextInput } from "flowbite-react";
+import { useEffect, useRef, useState } from "react";
 import { useSelector } from "react-redux"
-
-
+import {
+  getDownloadURL,
+  getStorage,
+  ref,
+  uploadBytesResumable,
+} from 'firebase/storage';
+import { app } from '../firebase';
+import { CircularProgressbar } from 'react-circular-progressbar';
+import 'react-circular-progressbar/dist/styles.css';
 export default function DashProfile() {
   const {currenUser} = useSelector(state => state.user)
-  console.log(currenUser.profilePicture);
+  const [imageFileUrl,setImageFileUrl] = useState(null)
+  const [imageFile ,setimageFile] = useState(null)
+  const [imageFileUploadProgress, setImageFileUploadProgress] = useState(null);
+  const [imageFileUploadError, setImageFileUploadError] = useState(null);
+
+
+  const filePicker = useRef();
+  const handleChangeImage = (e)=>{
+    const file = e.target.files[0];
+    if(file){
+      setimageFile(file)
+      setImageFileUrl(URL.createObjectURL(file));
+    }
+  
+  }
+  useEffect(() => {
+    if (imageFile) {
+      uploadImage();
+    }
+  // eslint-disable-next-line react-hooks/exhaustive-deps
+  }, [imageFile]);
+
+  const uploadImage = async () => {
+    // service firebase.storage {
+    //   match /b/{bucket}/o {
+    //     match /{allPaths=**} {
+    //       allow read;
+    //       allow write: if 
+    //       request.resource.size < 2 * 1024 * 1024 &&
+    //       request.resource.contentType.matches('image/.*');
+    //     }
+    //   }
+    // }
+    const storage = getStorage(app);
+    const fileName = new Date().getTime() + imageFile.name;
+    const storageRef = ref(storage,fileName)
+    const uploadTask = uploadBytesResumable(storageRef, imageFile);
+    uploadTask.on(
+      'state_changed',
+      (snapshot) => {
+        const progress =
+          (snapshot.bytesTransferred / snapshot.totalBytes) * 100;
+
+        setImageFileUploadProgress(progress.toFixed(0));
+      },
+      // eslint-disable-next-line no-unused-vars
+      (error) => {
+        setImageFileUploadError(
+          'ไฟล์ที่เลือกไม่ใช่รูปภาพ (หรือไฟล์มีขนาดเกิน 2MB)'
+        );
+        setImageFileUploadProgress(null);
+        setimageFile(null);
+        setImageFileUrl(null);
+      },
+      ()=>{
+        getDownloadURL(uploadTask.snapshot.ref).then((downloadURL) => {
+        setImageFileUrl(downloadURL);
+        
+      });
+    }
+  );
+};
+
   return (
     <div className="max-w-lg w-full mx-auto min-h-screen">
       <div className=" w-full h-full text-center">
         <h1 className="text-3xl font-bold mt-10">Profile</h1>
-        <form className="flex flex-col gap-4 mt-4">
-            <div className="w-60 h-80 flex-col mx-auto my-8">
-              <input type="file" />
-              <img className="rounded-full w-full border-amber-50 border-4" src={currenUser.profilePicture} alt="" />
+        <form className="flex flex-col gap-4 ">
+            <input type="file" 
+            accept="image/*" 
+            onChange={handleChangeImage} 
+            ref={filePicker} 
+            hidden
+              />
+            <div className="relative w-32 h-32 self-center cursor-pointer shadow-md overflow-hidden rounded-full"   onClick={() => filePicker.current.click()} >
+           
+
+              {imageFileUploadProgress && (
+              <CircularProgressbar
+                  value={imageFileUploadProgress || 0}
+                  text={`${imageFileUploadProgress}%`}
+                  strokeWidth={5}
+                  styles={{
+                    root: {
+                      width: '100%',
+                      height: '100%',
+                      position: 'absolute',
+                      top: 0,
+                      left: 0,
+                    },
+                    path: {
+                      stroke: `rgba(62, 152, 199, ${
+                        imageFileUploadProgress / 100
+                      })`,
+                    },
+                  }}
+                />
+              )}
+              <img 
+                className={`rounded-full w-full h-full dark:border-amber-50 bg-contain border-slate-500 border-4 object-cover cursor-pointer 
+                ${
+                  imageFileUploadProgress &&
+                  imageFileUploadProgress < 100 &&
+                  'opacity-60'
+                }`}
+                src={imageFileUrl || currenUser.profilePicture} 
+                alt="" 
+           
+              />
             </div>  
+              {imageFileUploadError &&
+                 <Alert color='red'>{imageFileUploadError}</Alert>
+              }
               <TextInput 
                 id='username'
                 placeholder="user" 
