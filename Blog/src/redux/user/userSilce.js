a02124314c59a2cd05d351c257708f0c81e150e4
werapol <r5r5wa55@hotmail.com>
2024-02-22 15:11:44 +0700

    add admin and fix bug upload

--- Blog/src/redux/user/userSilce.js
+++ Blog/src/redux/user/userSilce.js
@@ -24,16 +24,16 @@ const initialState = {
         state.loading = false;
         state.error = action.payload;
       },
-      updateStart: (state) => {
+      upDateStart: (state) => {
         state.loading = true;
         state.error = null;
       },
-      updateSuccess: (state, action) => {
+      upDateSuccess: (state, action) => {
         state.currenUser = action.payload;
         state.loading = false;
         state.error = null;
       },
-      updateFailure: (state, action) => {
+      upDateFailure: (state, action) => {
         state.loading = false;
         state.error = action.payload;
       },
