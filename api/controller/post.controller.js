876c35f25e09bee0fc6651af1f76f26d1f102df5
werapol <r5r5wa55@hotmail.com>
2024-02-26 12:50:18 +0700

    bugfix post controller

--- api/controller/post.controller.js
+++ api/controller/post.controller.js
@@ -13,7 +13,7 @@ export const create = async (req,res,next)=>{
 
     }
     const slug = req.body.title.split(' ').join('-').toLowerCase().replace(/[^a-zA-Z0-9ก-๙]/g, '-');
-    const newPost  = new post ( {
+    const newPost  = new Post ( {
         ...req.body,slug,userId:req.user.id
     })
     try {
