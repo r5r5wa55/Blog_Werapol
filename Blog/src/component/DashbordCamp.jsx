1c305ba5e54a9d2b359e6dfddd2c0f6002f943be
werapol <r5r5wa55@hotmail.com>
2024-03-06 14:18:27 +0700

    admin dash

--- Blog/src/component/DashbordCamp.jsx
+++ Blog/src/component/DashbordCamp.jsx
@@ -129,7 +129,7 @@ export default function DashbordCamp() {
 
         
         <div className='flex flex-wrap gap-4 py-3 mx-auto justify-center'>
-            <div className='flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800'>
+            <div className='flex flex-col w-full sm:w-full md:w-full lg:w-auto shadow-md p-2 rounded-md dark:bg-gray-800'>
             <div className='flex justify-between  p-3 text-sm font-semibold'>
                 <h1 className='text-center p-2'>Recent users</h1>
                 <Button outline gradientDuoTone='purpleToPink'>
@@ -158,7 +158,7 @@ export default function DashbordCamp() {
                 ))}
             </Table>
             </div>
-            <div className='flex flex-col w-full md:w-auto shadow-md p-2 rounded-md dark:bg-gray-800'>
+            <div className='flex flex-col w-full  md:w-auto  shadow-md p-2 rounded-md dark:bg-gray-800'>
             <div className='flex justify-between  p-3 text-sm font-semibold'>
                 <h1 className='text-center p-2'>Recent comments</h1>
                 <Button outline gradientDuoTone='purpleToPink'>
