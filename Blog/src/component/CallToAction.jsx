2e662bc51dfda444bb7153da36436f1fc5349ec6
werapol <r5r5wa55@hotmail.com>
2024-03-06 14:42:48 +0700

    calltoaciton set

--- Blog/src/component/CallToAction.jsx
+++ Blog/src/component/CallToAction.jsx
@@ -3,25 +3,25 @@ import { Button } from "flowbite-react";
 
 export default function CallToAction() {
   return (
-    <div>
-        <div className="mx-auto border-2 rounded-tr-3xl rounded-bl-3xl lg:flex border-blue-400">
-            <div className="text-center flex flex-1 flex-col items-center justify-center">
+
+        <div className=" flex flex-col sm:flex-row p-3 border  w-fullborder-teal-500 justify-center items-center rounded-tl-3xl rounded-br-3xl text-center">
+            <div className="flex-1 justify-center flex flex-col w-full">
                <div className="mt-10 lg:m-0 font-bold text-xl">
                     Go to GitHub 
                </div>
                Check Project React 
                <div className="w-full">
-                <Button gradientDuoTone='purpleToPink' className=" w-5/6 mx-auto mt-5">Go </Button>
+                <Button gradientDuoTone='purpleToPink' className=" sm:w-full mx-auto mt-5 w-5/6">Go </Button>
                </div>
             </div>
-            <div className="">
+            <div className="flex-1 p-7">
                 <img 
                     src='https://github.githubassets.com/assets/social-2deb6d7d43e7.jpg' 
                     alt="" 
-                    className="sm:max-w-xl mx-auto w-full p-8"
+                    
                 />  
             </div>
         </div>
-    </div>
+
   )
 }
