d82bd3c7e6425f8cac0cc6f0595efc10e503049d
werapol <r5r5wa55@hotmail.com>
2024-03-08 15:51:13 +0700

    commit

--- api/index.js
+++ api/index.js
@@ -35,7 +35,7 @@ app.use('/api/comment',commentRouters);
 app.use(express.static(path.join(__dirname,'/client/dist')))
 
 app.get('*',(req,res)=>{
-    res.sendFile(path.join(__dirname,'client','dist','index.html'))
+    res.sendFile(path.join(__dirname,'client','dist',''))
 })
 
 app.use((err,req,res,next)=>{
