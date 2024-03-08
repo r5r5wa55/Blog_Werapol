679a927fbf12ad1820083e52ebb5ceffb9a8a9cc
werapol <r5r5wa55@hotmail.com>
2024-03-08 16:44:57 +0700

    fix bug

--- api/index.js
+++ api/index.js
@@ -32,10 +32,10 @@ app.use('/api/user',userRouters);
 app.use('/api/auth',authRouters);
 app.use('/api/post',postRouters);
 app.use('/api/comment',commentRouters);
-app.use(express.static(path.join(__dirname, '/client/dist')));
+app.use(express.static(path.join(__dirname, '/Blog/dist')));
 
 app.get('*', (req, res) => {
-  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+  res.sendFile(path.join(__dirname, 'Blog', 'dist', 'index.html'));
 });
 
 
